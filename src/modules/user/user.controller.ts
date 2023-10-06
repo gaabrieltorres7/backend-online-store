11445678b8ffdd35fb23de061cb30e23fd4d8417
Gabriel Torres <gaabrieltt7@gmail.com>
2023-10-05 22:41:27 -0300

    feat: user controller

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from 'src/modules/user/user.service';
+import { CreateUserDTO } from './dto/user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async create(@Body() data: CreateUserDTO) {
+    const user = this.userService.createUser(data);
+    const newUser = {
+      ...user,
+      password: undefined,
+    };
+    return newUser;
+  }
+}
