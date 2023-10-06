a8c3f6fddaeee01c9c4d757070332808ab51d9fb
Gabriel Torres <gaabrieltt7@gmail.com>
2023-10-05 22:41:12 -0300

    feat: userPrismaRepository

---
+++ src/modules/user/repositories/prisma/user-prisma-repository.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/infra/db/prisma.service';
+import { CreateUserDTO, UserCreatedDTO } from '../../dto/user.dto';
+import { IUserRepository } from '../user-interface';
+
+@Injectable()
+export class UserPrismaRepository implements IUserRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async save(data: CreateUserDTO): Promise<UserCreatedDTO> {
+    return await this.prisma.user.create({ data });
+  }
+}
