2f3875557f687a51eeb0d52c28701d111198a51c
Gabriel Torres <gaabrieltt7@gmail.com>
2023-10-07 15:46:14 -0300

    feat: StateModule, CityModule, AddressModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AddressModule } from './modules/address/address.module';
+import { CityModule } from './modules/city/city.module';
+import { StateModule } from './modules/state/state.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, StateModule, CityModule, AddressModule],
   controllers: [],
   providers: [],
 })
