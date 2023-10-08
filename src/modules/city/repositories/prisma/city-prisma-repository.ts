d5b357b29927e0f74dbb1694b1c544834fdbb61e
Gabriel Torres <gaabrieltt7@gmail.com>
2023-10-08 11:48:04 -0300

    feat: cityPrismaRepository, getAllCitiesByStateId

---
+++ src/modules/city/repositories/prisma/city-prisma-repository.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/infra/db/prisma.service';
+import { CityDTO } from '../../dto/city.dto';
+import { ICityRepository } from '../city-interface';
+
+@Injectable()
+export class CityPrismaRepository implements ICityRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async getAllCitiesByStateId(stateId: number): Promise<CityDTO[]> {
+    const cities = await this.prisma.city.findMany({
+      where: {
+        stateId,
+      },
+      include: {
+        Addresses: true,
+      },
+    });
+    return cities;
+  }
+}
