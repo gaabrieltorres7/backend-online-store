452c979d6f91cfaf6fcef43b5be618a7cf783164
Gabriel Torres <gaabrieltt7@gmail.com>
2023-12-12 21:24:42 -0300

    test: more category tests

--- src/modules/category/category.service.spec.ts
+++ src/modules/category/category.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoryService } from './category.service';
 import { ICategoryRepository } from './repositories/category-interface';
@@ -25,13 +26,39 @@ describe('CategoryService', () => {
   });
 
   it('should be able to return all categories', async () => {
+    await sut.create({ name: 'Category 1' });
     const result = await sut.findAll();
     expect(result).toHaveLength(1);
   });
 
   it('should not be able to return all categories if not exists', async () => {
-    const result = await sut.findAll();
-    // await expect(result.slice(1)).rejects.toBeInstanceOf(NotFoundException); ///fix this when it has a create function
-    expect(result.slice(1)).toEqual([]);
+    expect(async () => await sut.findAll()).rejects.toBeInstanceOf(
+      HttpException,
+    );
+  });
+
+  it('should be able to return an error if category name already exists', async () => {
+    await sut.create({ name: 'Category 1' });
+    expect(sut.create({ name: 'Category 1' })).rejects.toBeInstanceOf(
+      HttpException,
+    );
+  });
+
+  it('should be able to create a category', async () => {
+    const category = await sut.create({ name: 'Category 1' });
+
+    expect(category).toHaveProperty('id');
+    expect(category.name).toEqual('Category 1');
+  });
+
+  it('should be able to return a category by name', async () => {
+    await sut.create({ name: 'Category 1' });
+    const category = await sut.findByName('Category 1');
+
+    expect(category.name).toEqual('Category 1');
+  });
+
+  it('should not be able to return a category if it not exists', async () => {
+    expect(sut.findByName('test')).rejects.toBeInstanceOf(HttpException);
   });
 });
